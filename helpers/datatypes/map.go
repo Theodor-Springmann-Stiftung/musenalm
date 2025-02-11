8ded984d01f9f7ba4b261aaf674ce0fa0a1ff723
Simon Martens <simon.martens@mailbox.org>
2025-02-11 22:06:58 +0100

    Initiatlize the map right

--- helpers/datatypes/map.go
+++ helpers/datatypes/map.go
@@ -2,7 +2,7 @@ package datatypes
 
 // INFO: use this if your key is unique
 func MakeMap[T any, U comparable](data []T, f func(T) U) map[U]T {
-	m := make(map[U]T)
+	m := make(map[U]T, len(data))
 	for _, v := range data {
 		m[f(v)] = v
 	}
@@ -11,7 +11,7 @@ func MakeMap[T any, U comparable](data []T, f func(T) U) map[U]T {
 
 // INFO: use this if your key is not unique
 func MakeMultiMap[T any, U comparable](data []T, f func(T) U) map[U][]T {
-	m := make(map[U][]T)
+	m := make(map[U][]T, len(data))
 	for _, v := range data {
 		m[f(v)] = append(m[f(v)], v)
 	}
